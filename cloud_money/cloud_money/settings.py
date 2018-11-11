0aa2e73eb253a1a6701dbf9faf0523a28741889e
akerukesha <okapova.akerke@gmail.com>
2018-11-11 06:44:05 +0600

    changed requests version

--- cloud_money/cloud_money/settings.py
+++ cloud_money/cloud_money/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'cloud_money.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +68,9 @@ TEMPLATES = [
         },
     },
 ]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 WSGI_APPLICATION = 'cloud_money.wsgi.application'
 
